8a621ff1843276711b93d666c48da479db6d79cc
Rahul Chandra <rahulshaku2@gmail.com>
2023-06-28 23:04:39 -0400

    Adding prep.sh to find the location of autobuild.conf

---
+++ prep.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This sets the location for the autobuild.conf
+LOCATION=$(readlink -f autobuild.conf)
+FILES=$(grep -r 'source autobuild.conf' | sed 's/:.*//g')
+echo "$FILES" | while IFS= read -r file; do
+   sed -i "s/autobuild.conf/$LOCATION/g" file
+done
+
