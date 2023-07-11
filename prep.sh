a1379b11c0398c9483b2fdbd7fcfa398b59770df
Rahul Chandra <rahulshaku2@gmail.com>
2023-07-10 21:49:22 -0400

    Follow up previous phase out

--- prep.sh
+++ prep.sh
@@ -5,4 +5,6 @@ FILES=$(grep -r 'source autobuild.conf' | sed 's/:.*//g')
 echo "$FILES" | while IFS= read -r file; do
    sed -i "s|autobuild\.conf|$LOCATION|g" $file
 done
+# Set Working direcotry 
+sed -i "s/ROOT=.*/ROOT=$(pwd)/" autobuild.conf
 
