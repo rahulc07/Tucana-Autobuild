c42728e366afd7317f86bc9f3d3ce96c7583503f
Rahul Chandra <rahulshaku2@gmail.com>
2023-09-16 00:30:35 -0400

    Add the API key back as a variable

--- currency-scrapers/icu.sh
+++ currency-scrapers/icu.sh
@@ -9,7 +9,7 @@ fi
 REPO=$(cat $(find . -name $PACKAGE) | grep URL | head -1 | sed 's/.*\.com//' | sed 's|/|!|3' | sed 's/!.*//' | sed 's/^.//')
 LATEST_VER=$(curl -Ls \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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 "\
+  -H "Authorization: Bearer $GITHUB_API_KEY "\
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$REPO/releases/latest | grep tag_name | sed 's/.$//' | sed 's/\"//g' | sed 's/.*://' | sed 's/^.//' | sed 's/release-//g' | sed 's/v//' | sed 's/-/\./g')
 # Some apps have TAG_OVERRIDE because the tags are newer than the releases, in that case use those instead
@@ -22,7 +22,7 @@ fi
 if [[ -z $LATEST_VER ]]; then
   LATEST_VER=$(curl -Ls \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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 "\
+  -H "Authorization: Bearer $GITHUB_API_KEY "\
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$REPO/tags | grep name | head -1 | sed 's/.$//' | sed 's/\"//g' | sed 's/.*://' | sed 's/^.//' | sed 's/.*-//g' | sed 's/v//')
 fi
