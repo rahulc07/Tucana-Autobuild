fb31b392eca671756656f0bc7d016088bdd320f6
Rahul Chandra <rahulshaku2@gmail.com>
2023-11-05 13:36:57 -0500

    Add new python modules and fix ruby

--- currency-scrapers/ruby.sh
+++ currency-scrapers/ruby.sh
@@ -7,7 +7,7 @@ LATEST_VER=$(curl -Ls \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_API_KEY "\
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$REPO/releases/latest | grep tag_name | sed 's/.$//' | sed 's/\"//g' | sed 's/.*://' | sed 's/^.//' | sed 's/.*-//g' | sed 's/v//')
+  https://api.github.com/repos/$REPO/releases/latest | grep tag_name | sed 's/.$//' | sed 's/\"//g' | sed 's/.*://' | sed 's/^.//' | sed 's/.*-//g' | sed 's/v//' | sed 's/_/\./g')
 # Some apps have TAG_OVERRIDE because the tags are newer than the releases, in that case use those instead
 cat $(find . -name $PACKAGE -not -path './.git*') | grep TAG_OVERRIDE &> /dev/null
 if [[ $? == 0 ]]; then
