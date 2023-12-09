e1e9c01d1e0190c65f24be65805ee7f207b7c93a
Rahul Chandra <rahulshaku2@gmail.com>
2023-12-09 19:42:56 +0000

    Add webkitgtk currency and fixes

---
+++ currency-scrapers/perl.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+source autobuild.conf
+PACKAGE=$1
+cd $BUILD_SCRIPTS_ROOT
+REPO="Perl/perl5"
+LATEST_VER=$(curl -Ls \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_API_KEY "\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$REPO/releases/latest | grep tag_name | sed 's/.$//' | sed 's/\"//g' | sed 's/.*://' | sed 's/^.//' | sed 's/.*-//g' | sed 's/v//')
+# Some apps have TAG_OVERRIDE because the tags are newer than the releases, in that case use those instead
+cat $(find . -name $PACKAGE -not -path './.git*') | grep TAG_OVERRIDE &> /dev/null
+if [[ $? == 0 ]]; then
+   LATEST_VER=""
+fi
+
+
+if [[ -z $LATEST_VER ]]; then
+  LATEST_VER=$(curl -Ls \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_API_KEY "\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$REPO/tags | grep name | grep '5.38' | head -1 | sed 's/.$//' | sed 's/\"//g' | sed 's/.*://' | sed 's/^.//' | sed 's/.*-//g' | sed 's/v//')
+fi
+echo $LATEST_VER
